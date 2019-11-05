e7d926f968354cee94044a3e8fb9ae789440b2ac
Julia <Julia@Johans-MacBook-Pro.local>
2019-11-05 15:12:08 +0100

    la till python beräkningspogram

---
+++ math with python/funktioner.py
@@ -0,0 +1,17 @@
+def y(x):
+    svar = 6 * x - 2
+    print (svar)
+    return svar
+
+y(8) - y(5)
+svar = 46 - 28 
+print (svar)
+
+def y(t):
+    svar = 140*t + 120
+    print (svar)
+    return svar
+
+y(0)
+y(5)
+
---
+++ math with python/math_tasks.py
@@ -0,0 +1,27 @@
+import math
+
+print ("hello world")
+svar1 = 3 ** 2 + 4 ** 3
+print (svar1)
+svar2 = 2 ** 3 + 3 ** 4
+print (svar2)
+
+
+#triangeluppgift
+
+katet1 = 3
+katet2 = 2
+hypotenusa = math.sqrt(katet1 ** 2 + katet2 ** 2 )
+
+print("answer to the question:" + str(hypotenusa))
+
+#kateterna
+hypotenusa1 = math.sqrt (74)
+hypotenusa2 = math.sqrt (7 ** 2 + 5 ** 2)
+hypotenusa3 = math.sqrt (7 ** 2 + 7 ** 2)
+hypotenusa4 = math.sqrt (8 ** 2 + 6 ** 2)
+
+print (hypotenusa1)
+print (hypotenusa2)
+print (hypotenusa3)
+print (hypotenusa4)
