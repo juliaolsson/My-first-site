8deecbcd60545a9246c6ce84f01bf71cc8ce7a87
Julia <Julia@Johans-MacBook-Pro.local>
2019-11-06 15:58:22 +0100

    hej, måste gå nu

--- backend.py
+++ backend.py
@@ -1,9 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/api/restart')
+def restart():
+    print ("restart")
+    # lägg till kod som startar om filmen
+    return 'restart'
+
+@app.route('/api/ON')
+def ON():
+    print ("ON")
+    # lägg till kod som startar filmen
+    return "ON"
+
+@app.route('/api/OFF')
+def OFF():
+    print ("OFF")
+    # lägg till kod som avslutar filmen
+    return "OFF"
+
+
 @app.route('/')
 def hello_world():
     print ("hello world")
-    return 'Hello, World!'
+    return app.send_static_file("index.html")
 
-app.run ('localhost', 8080)
+app.run ('172.20.10.3', 8080)
